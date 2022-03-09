b4f376809555bc2d28ada373b14530aa1f9d5028
kuralsiz47 <98223394+kuralsiz47@users.noreply.github.com>
2022-03-09 22:55:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
-UPSTREAM_REPO = "https://github.com/Pulsar8806/pulsar"
+UPSTREAM_REPO = "https://github.com/kuralsiz47/yukkiyeni"
 UPSTREAM_BRANCH = "main"
 
 SUPPORT_CHANNEL = "https://t.me/Sohbetdestek"
